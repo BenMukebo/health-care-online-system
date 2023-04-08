405fcbd919f537ec207e673051a266793dbe6045
Alfredbis29 <Kalumunabisimwa5@gmail.com>
2023-04-08 17:52:03 +0200

    Define new action to initialize a new hospital instance

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -2,8 +2,13 @@ class HospitalsController < ApplicationController
   def index
     @hospitals = Hospital.all
   end
+
   def show
     @hospital = Hospital.find(params[:id])
+  end
+
+  def new
+    @hospital = Hospital.new
   end
   
 end
