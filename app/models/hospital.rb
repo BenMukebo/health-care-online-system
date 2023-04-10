423b2042206b6873a636129fd49f26302192fb69
Alfredbis29 <Kalumunabisimwa5@gmail.com>
2023-04-10 17:10:13 +0200

    create the hospital model

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,10 +1,4 @@
 class Hospital < ApplicationRecord
-  belongs_to :user
-  has_many :treatments
-  has_many :healthcare_requests
-
   validates :name, presence: true
   validates :address, presence: true
-  validates :treatments_counter, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :healthcare_requests_counter, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
