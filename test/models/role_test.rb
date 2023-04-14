158a4e865725811b4a7b1d074f5dbcbd35f40362
BenMukebo <l.benkasmukebo7@gmail.com>
2023-04-14 13:28:48 +0200

    Associate a Role to a User and  add association tests

--- test/models/role_test.rb
+++ test/models/role_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class RoleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should have_many(:users)
+
   should validate_presence_of(:name)
   should validate_uniqueness_of(:name)
 end
