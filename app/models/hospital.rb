59e2f661b823cd0b6287033246361b89c2aa2ca6
Alfredbis29 <Kalumunabisimwa5@gmail.com>
2023-04-14 15:38:09 +0200

    add the form

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,4 +1,5 @@
 class Hospital < ApplicationRecord
+  enum status: { active: 0, inactive: 1 }
   validates :name, presence: true
   validates :address, presence: true
 end
--- db/migrate/20230407202008_create_hospitals.rb
+++ db/migrate/20230407202008_create_hospitals.rb
@@ -1,9 +1,9 @@
 class CreateHospitals < ActiveRecord::Migration[7.0]
   def change
     create_table :hospitals do |t|
-      t.string :name
-      t.jsonb :address,  null: false, default: '{}'
-      t.jsonb :data, null: false, default: '{}'
+      t.string :name, null: false
+      t.jsonb :address, null: false, default: {}
+      t.jsonb :data, default: {}
       t.integer :status, default: 0, null: false
       t.references :user, null: false, foreign_key: true
 
