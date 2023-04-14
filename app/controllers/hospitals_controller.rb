a1d093e9820263f16be48d894f593bb45e3b47e8
Alfredbis29 <Kalumunabisimwa5@gmail.com>
2023-04-14 12:32:23 +0200

    fix linters

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -11,51 +11,52 @@ class HospitalsController < ApplicationController
     @hospital = Hospital.new
   end
 
-def create
-  @hospital = Hospital.new(hospital_params)
+  def create
+    @hospital = Hospital.new(hospital_params)
 
-  if @hospital.save
-    redirect_to @hospital
-  else
-    render :new
+    if @hospital.save
+      redirect_to @hospital
+    else
+      render :new
+    end
   end
-end
 
-def edit
-  @hospital = Hospital.find(params[:id])
-end
-
-def update
-  @hospital = Hospital.find(params)
-
-if @hospital.update(hospital_params)
-  redirect_to @hospital
-else
-  render :edit
- end
-end
-
-def destroy
-  @hospital = Hospital.find(params[:id])
-  @hospital.destroy
-
-  redirect_to hospitals_path
-end
-
-private
-
-def hospital_params
-  params.require(:hospital).permit(:name,:address => %i[country city state zip_code],
-    :data => {
-      :treatments => [:string],
-      :healthcare_requests => [:string],
-      :staff => [:string],
-      :rooms => [:integer],
-      :ratings => [:integer]
-    },
-    :status
-    :user_id
-  )
+  def edit
+    @hospital = Hospital.find(params[:id])
   end
- end
+
+  def update
+    @hospital = Hospital.find(params[:id])
+
+    if @hospital.update(hospital_params)
+      redirect_to @hospital
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @hospital = Hospital.find(params[:id])
+    @hospital.destroy
+
+    redirect_to hospitals_path
+  end
+
+  private
+
+  def hospital_params
+    params.require(:hospital).permit(
+      :name,
+      :status,
+      :user_id,
+      address: %i[country city state zip_code],
+      data: {
+        treatments: [:string],
+        healthcare_requests: [:string],
+        staff: [:string],
+        rooms: [:integer],
+        ratings: [:integer]
+      }
+    )
+  end
 end
