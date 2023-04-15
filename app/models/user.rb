7d88e0460734f59ab59a22745730dff372b5597e
BenMukebo <l.benkasmukebo7@gmail.com>
2023-04-15 21:44:31 +0200

    use SCREAMING_SNAKE_CASE for stutus constants

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,6 @@ class User < ApplicationRecord
   validates :phone, uniqueness: true, length: { within: 10..16 }, allow_blank: true
   # validates :matricule_number, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates_numericality_of :matricule_number, only_integer: true, greater_than_or_equal: 0, allow_blank: true
-  # enum marital_status: { single: 0, married: 1, widowed: 2, divorced: 3, separated: 4 }, _default: 'single'
 
   enum marital_status: { single: 0, married: 1, widowed: 2, divorced: 3, separated: 4 }, _default: 'single'
   validates :marital_status, inclusion: { in: marital_statuses.keys }, allow_blank: true
@@ -20,15 +19,15 @@ class User < ApplicationRecord
   enum genders: { Male: 'M', Female: 'F' }
   validates :gender, inclusion: { in: genders.keys }, allow_blank: true
 
-  Status = { inactive: 0, active: 1, break: 3 }.freeze
-  enum status: Status
+  STATUS = { inactive: 0, active: 1, rest: 3 }.freeze
+  enum status: STATUS
 
   validate do
-    errors.add(:status, "invalid status, please choose one of: #{Status.keys.join(', ')}") if @not_valid_status
+    errors.add(:status, "invalid status, please choose one of: #{STATUS.keys.join(', ')}") if @not_valid_status
   end
 
   def status=(value)
-    if Status.include?(value.to_sym)
+    if STATUS.include?(value.to_sym)
       super value
     else
       @not_valid_status = true
