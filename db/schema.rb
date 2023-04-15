27ea648187d78f7ed3e8f1292c72338c44044611
BenMukebo <l.benkasmukebo7@gmail.com>
2023-04-15 18:35:06 +0200

    Creta a new migration to add records to the user model

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_15_132731) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
+    t.string "first_name"
     t.string "familly_name"
     t.string "middle_name"
     t.integer "matricule_number"
