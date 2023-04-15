4e2ef1c5278c6c8ca55f0cd38f8368739530e24d
BenMukebo <l.benkasmukebo7@gmail.com>
2023-04-15 23:44:42 +0200

    Add hospital validations

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,5 +1,24 @@
 class Hospital < ApplicationRecord
-  enum status: { active: 0, inactive: 1 }
   validates :name, presence: true
+  validates_presence_of :name
+  validates_uniqueness_of :name
   validates :address, presence: true
+
+  # validates :data, presence: true, length: { maximum: 9 }
+  # validates :address, presence: true, length: { maximum: 4 }
+
+  STATUS = { inactive: 0, active: 1 }.freeze
+  enum status: STATUS
+
+  validate do
+    errors.add(:status, "invalid status, please choose one of: #{STATUS.keys.join(', ')}") if @not_valid_status
+  end
+
+  def status=(value)
+    if STATUS.include?(value.to_sym)
+      super value
+    else
+      @not_valid_status = true
+    end
+  end
 end
