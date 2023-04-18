366da29f323b184e9f881dab196409f6c4772d39
BenMukebo <l.benkasmukebo7@gmail.com>
2023-04-18 20:58:56 +0200

    Remove the name validation method

--- app/models/role.rb
+++ app/models/role.rb
@@ -3,18 +3,6 @@ class Role < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
 
-  NAME = { Guest: 0, Client: 1, Employee: 2, Manager: 3, Admin: 4, Superadmin: 5 }.freeze
-  enum name: NAME
-
-  validate do
-    errors.add(:name, "invalid name, please choose one of: #{NAME.keys.join(', ')}") if @not_valid_name
-  end
-
-  def name=(value)
-    if NAME.include?(value.to_sym)
-      super value
-    else
-      @not_valid_name = true
-    end
-  end
+  NAME = { guest: 0, client: 1, employee: 2, manager: 3, admin: 4, superAdmin: 5 }.freeze
+  enum name: NAME, _default: 0
 end
