97c2009999b3b980ac7a8865dae3ed8549fc2f18
BenMukebo <l.benkasmukebo7@gmail.com>
2023-04-21 03:22:30 +0200

    dd orginization model input validation

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,8 @@
 class Organization < ApplicationRecord
+  # has_many :users
+
+  enum status: { inactive: 0, active: 1 }.freeze
+
+  validates :name, presence: true
+  validates_presence_of :status
 end
