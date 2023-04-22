f732f7163673d64222dd3069993b6ab951c4b623
BenMukebo <l.benkasmukebo7@gmail.com>
2023-04-22 20:50:14 +0200

    Update organization and hospital model validation

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,10 +1,12 @@
 class Hospital < ApplicationRecord
   # has_many :users
 
-  enum status: { inactive: 0, active: 1 }.freeze
+  enum status: { inactive: 0, active: 1, under_investigation: 2 }.freeze
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates_presence_of :status
+  validates_uniqueness_of :email, :phone_number, :register_number
+
   # validates :data, presence: true, length: { maximum: 9 }
   # validates :address, presence: true, length: { maximum: 4 }
 end
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,8 +1,9 @@
 class Organization < ApplicationRecord
   # has_many :users, dependent: :destroy
 
-  enum status: { inactive: 0, active: 1 }.freeze
+  enum status: { inactive: 0, active: 1, under_investigation: 2 }.freeze
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates_presence_of :status
+  validates_uniqueness_of :email, :phone_number, :register_number
 end
