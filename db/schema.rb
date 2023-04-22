b49e97d0c0b81b06b570c416af03fc55e47940dd
BenMukebo <l.benkasmukebo7@gmail.com>
2023-04-22 20:33:43 +0200

    Update schema database

--- db/schema.rb
+++ db/schema.rb
@@ -10,36 +10,63 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_21_011322) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_22_131319) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "contracts", force: :cascade do |t|
+    t.bigint "organization_id", null: false
+    t.bigint "hospital_id", null: false
+    t.string "start_date"
+    t.string "end_date"
+    t.jsonb "terms_of_agreement", default: {}, null: false
+    t.integer "agreement_type"
+    t.integer "status", default: 0, null: false
+    t.string "legal_document"
+    t.boolean "renewal_option"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["hospital_id"], name: "index_contracts_on_hospital_id"
+    t.index ["organization_id"], name: "index_contracts_on_organization_id"
+    t.index ["terms_of_agreement"], name: "index_contracts_on_terms_of_agreement", using: :gin
+  end
+
   create_table "hospitals", force: :cascade do |t|
     t.string "name", null: false
-    t.jsonb "address", default: {}, null: false
+    t.string "email"
+    t.string "phone_number"
+    t.string "register_number", limit: 8
+    t.string "logo"
+    t.string "website"
     t.jsonb "data", default: {}, null: false
+    t.jsonb "address", default: {}, null: false
+    t.text "terms_of_service"
     t.integer "status", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["address"], name: "index_hospitals_on_address", using: :gin
     t.index ["data"], name: "index_hospitals_on_data", using: :gin
+    t.index ["email"], name: "index_hospitals_on_email"
     t.index ["name"], name: "index_hospitals_on_name"
     t.index ["status"], name: "index_hospitals_on_status"
   end
 
   create_table "organizations", force: :cascade do |t|
     t.string "name", null: false
-    t.string "logo"
+    t.string "email"
+    t.string "phone_number"
     t.string "register_number", limit: 8
+    t.string "logo"
+    t.string "website"
     t.jsonb "data", default: {}, null: false
     t.jsonb "location", default: {}, null: false
+    t.text "terms_of_service"
     t.integer "status", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["data"], name: "index_organizations_on_data", using: :gin
     t.index ["location"], name: "index_organizations_on_location", using: :gin
     t.index ["name"], name: "index_organizations_on_name"
-    t.index ["register_number"], name: "index_organizations_on_register_number", unique: true
     t.index ["status"], name: "index_organizations_on_status"
   end
 
@@ -70,7 +97,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_21_011322) do
     t.jsonb "data", default: "{}", null: false
     t.jsonb "address", default: "{}", null: false
     t.jsonb "phyisical_appearence", default: "{}", null: false
-    t.string "privacy_policy"
+    t.boolean "agreed_to_terms"
     t.integer "status", default: 0, null: false
     t.bigint "role_id", null: false
     t.index ["address"], name: "index_users_on_address", using: :gin
@@ -82,5 +109,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_21_011322) do
     t.index ["role_id"], name: "index_users_on_role_id"
   end
 
+  add_foreign_key "contracts", "hospitals"
+  add_foreign_key "contracts", "organizations"
   add_foreign_key "users", "roles"
 end
