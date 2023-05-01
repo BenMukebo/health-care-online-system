3e8082a601866153d5bc95fb28e1fd91986c68b7
BenMukebo <l.benkasmukebo7@gmail.com>
2023-05-01 18:19:56 +0200

    Add active_storage logo and description to the organization and hospital models

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -3,6 +3,9 @@ class Hospital < ApplicationRecord
   has_many :contracts, dependent: :destroy
   has_many :organizations, through: :contracts # , dependent: :destroy
 
+  has_one_attached :logo
+  has_rich_text :description
+
   enum status: { inactive: 0, active: 1, under_investigation: 2 }.freeze
 
   validates :name, presence: true, uniqueness: true
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,6 +3,9 @@ class Organization < ApplicationRecord
   has_many :contracts, dependent: :destroy
   has_many :hospitals, through: :contracts # , dependent: :destroy
 
+  has_one_attached :logo
+  has_rich_text :description
+
   enum status: { inactive: 0, active: 1, under_investigation: 2 }.freeze
 
   validates :name, presence: true, uniqueness: true
