61d8b9ca6572d1bc711c79a23fd7fc664e577dfb
BenMukebo <l.benkasmukebo7@gmail.com>
2023-05-16 14:53:55 +0200

    Comment the password db.yml and rmove logout home method
    Co-authored-by: Alfred Bisimwa <kalumunabisimwa5@gmail.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,4 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index; end
-
-  def logout
-    sign_out(current_user)
-    redirect_to root_path
-  end
 end
