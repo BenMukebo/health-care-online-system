e183a9695576c87764579a6db6aab79ae1450b0b
BenMukebo <l.benkasmukebo7@gmail.com>
2023-05-24 12:37:29 +0200

    Add some css configuration

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,12 +5,22 @@ module.exports = {
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*.{erb,haml,html,slim}',
+    './app/**/*.{html,js,erb}'
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'primary': '#171717',
+        'secondary': '#2719C5',
+        'white': '#FFFFFF',
+        'nav-color': 'rgba(255, 255, 255, 0.75',
+        'nav-bg': '#90BADD',
+        'footer-bg': '#0d0d0d',
+        'footer-color': '#d0daf5',
       },
     },
   },
