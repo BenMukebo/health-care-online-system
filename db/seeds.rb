a940f5e832ba89507aea547979acdb9331ae385a
BenMukebo <l.benkasmukebo7@gmail.com>
2023-05-26 23:47:09 +0200

    Create default role from the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-role = Role.find_or_create_by!(name: 'super_admin')
-super_admin = User.create!(first_name: "Kangu", familly_name: "mamba", email: "kangu@gmail.com", password: "password", agreed_to_terms: true, role_id: role.id)
+if Rails.env.development?
 
-# Attach default image to the super_admin_user
-admin_image_path = Rails.root.join('app', 'assets', 'images', 'logo.svg')
-super_admin.image.attach(io: File.open(admin_image_path), filename: 'logo.svg')
+  role1 = Role.find_or_create_by!(name: 'super_admin')
+  role2 = Role.find_or_create_by!(name: 'admin')
+  role3 = Role.find_or_create_by!(name: 'user')
+  role4 = Role.find_or_create_by!(name: 'employee')
+
+  superAdmin = User.create!(first_name: "Kangu", familly_name: "mamba", email: "kangu@gmail.com", password: "password", agreed_to_terms: true, role_id: role1.id)
+
+  image_path = Rails.root.join('app', 'assets', 'images', 'logo.svg')
+  superAdmin.image.attach(io: File.open(image_path), filename: 'logo.svg')
+
+  # image = File.open("#{Rails.root}/app/assets/images/logo.svg")
+  # superAdmin.image.attach(io: image, filename: "logo.svg")
+  # superAdmin.save!
+
+end
