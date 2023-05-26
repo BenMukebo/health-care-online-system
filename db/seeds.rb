8b4685b0d12d9a66e1fdb4ba8d8dbe688e9bfefb
BenMukebo <l.benkasmukebo7@gmail.com>
2023-05-26 23:49:16 +0200

    Create some users with data Faker generator from the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,4 +14,24 @@ if Rails.env.development?
   # superAdmin.image.attach(io: image, filename: "logo.svg")
   # superAdmin.save!
 
+  puts '----------------------USERS---------------------------'
+
+  30.times do |i|
+    User.create!(
+      email: Faker::Internet.unique.email,
+      password: '123456',
+      first_name: Faker::Name.first_name[0..10],
+      familly_name: Faker::Name.last_name[0..10],
+      phone: Faker::PhoneNumber.cell_phone,
+      agreed_to_terms: true,
+      role_id: role3.id,
+      marital_status: rand(0..4),
+      gender: Faker::Gender.binary_type,
+      address: { country: Faker::Address.country, city: Faker::Address.city, state: Faker::Address.state },
+      # confirmation_sent_at: DateTime.now,
+      # confirmed_at: DateTime.now + 1.hour,
+      # investment_per_interest: Faker::Number.between(from: 1, to: 10),
+    )
+  end
+
 end
