c6ced3a1f023f8445a865ff235f65ee3db7672c6
BenMukebo <l.benkasmukebo7@gmail.com>
2023-05-26 19:03:12 +0200

    Update the migration schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_03_182051) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_26_165500) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -136,10 +136,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_03_182051) do
     t.boolean "agreed_to_terms"
     t.integer "status", default: 0, null: false
     t.bigint "role_id", null: false
+    t.bigint "organization_id"
+    t.bigint "hospital_id"
     t.index ["address"], name: "index_users_on_address", using: :gin
     t.index ["data"], name: "index_users_on_data", using: :gin
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["hospital_id"], name: "index_users_on_hospital_id"
     t.index ["matricule_number"], name: "index_users_on_matricule_number", unique: true
+    t.index ["organization_id"], name: "index_users_on_organization_id"
     t.index ["phyisical_appearence"], name: "index_users_on_phyisical_appearence", using: :gin
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["role_id"], name: "index_users_on_role_id"
@@ -149,5 +153,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_03_182051) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "contracts", "hospitals"
   add_foreign_key "contracts", "organizations"
+  add_foreign_key "users", "hospitals"
+  add_foreign_key "users", "organizations"
   add_foreign_key "users", "roles"
 end
