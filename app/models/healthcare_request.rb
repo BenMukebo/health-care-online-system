6e23d04d910f186013517538bae9e3f5960aed6c
BenMukebo <l.benkasmukebo7@gmail.com>
2023-05-30 23:59:45 +0200

    Add status type enum vaidation an tile columns

--- app/models/healthcare_request.rb
+++ app/models/healthcare_request.rb
@@ -4,4 +4,9 @@ class HealthcareRequest < ApplicationRecord
   belongs_to :hospital
 
   has_rich_text :description
+
+  validates :tile, length: { maximum: 60 }, presence: true
+  enum status: { pending: 0, rejected: 1, approved: 2 }.freeze
+
+  validates_presence_of :status
 end
