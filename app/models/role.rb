2d1ec81228ea9266885cb97a5220956558c88ba3
BenMukebo <l.benkasmukebo7@gmail.com>
2023-06-01 17:50:31 +0200

    Update role enum remove the user default  value and add uniqueness validation

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,9 +1,8 @@
 class Role < ApplicationRecord
   has_many :users
 
-  NAME = %i[super_admin admin user employee manager client].freeze
-  enum name: NAME.freeze, _default: 'user'
+  NAME = %i[user doctor admin super_admin employee client].freeze
+  enum name: NAME.freeze # _default: 'user'
 
-  validates :name, presence: true, uniqueness: true
-  # validates :name, inclusion: { in: NAME }
+  validates :name, presence: true, inclusion: { in: names }, uniqueness: { case_sensitive: false }
 end
